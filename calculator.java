9ca21564dbfd733c881a157c5c3228aa74917393
Charbel Maroun <charbel.maroun@ucalgary.ca>
2024-02-08 14:32:19 -0700

    Added files

--- calculator.java
+++ calculator.java
@@ -1,8 +1,8 @@
-public class Calculator {
-    public int evaluate(String expression) {
-      int sum = 0;
-      for (String summand: expression.split("\\+"))
-        sum += Integer.valueOf(summand);
-      return sum;
-    }
+public class calculator {
+  public int evaluate(String expression) {
+    int sum = 0;
+    for (String summand : expression.split("\\+"))
+      sum += Integer.valueOf(summand);
+    return sum;
   }
+}
