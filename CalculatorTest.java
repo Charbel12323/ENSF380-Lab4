8956292680e1538a8b5b1bf239b00e5175415c99
Gisa Tchaka <gisa.gashagaza@ucalgary.ca>
2024-02-08 14:50:30 -0700

    ububu

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 public class CalculatorTest {
   @Test
   public void evaluatesExpression() {
-    Calculator calculator = new Calculator();
+    calculator calculator = new calculator();
     int sum = calculator.evaluate("1+2+3");
     assertEquals(6, sum);
   }
--- calculator.java
+++ calculator.java
@@ -1,4 +1,4 @@
-public class Calculator {
+public class calculator {
     public int evaluate(String expression) {
       int sum = 0;
       for (String summand: expression.split("\\+"))
